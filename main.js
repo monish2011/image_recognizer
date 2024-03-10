50849d86b59473bc5250dc4f56ea83ad1c152a18
monish2011 <142167411+monish2011@users.noreply.github.com>
2024-03-10 11:59:00 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -8,12 +8,27 @@ camera= document.getElementById("camera");
 Webcam.attach('#camera');
 function take_snapshot(){
 Webcam.snap(function(data_uri){
-    document.getElementById("result").innerHTML = '<img id="captured img" src="'+ data_uri +'"/>';
+    document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+ data_uri +'"/>';
 
 });
 }
 console.log('ml5 version :', ml5.version );
 classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/6AObGbhWI/model.json', modelLoaded);
 function modelLoaded(){
-    console.log('ModelLoaded!')
+    console.log('ModelLoaded!');
+}
+function check(){
+    img = document.getElementById('selfie_image');
+    classifier.classify(img, gotResult); 
+}
+function gotResult(error,results){
+    if (error){
+        console.error(error);
+
+    }
+    else{
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML= results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
 }
